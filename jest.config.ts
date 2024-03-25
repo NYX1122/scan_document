57c7d54a49b0e150e51855c2821d8d4a2aa879d3
BoDee Angus <bodee.angus@gmail.com>
2024-03-25 16:32:56 -0600

    bla

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  rootDir: './src',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  collectCoverageFrom: ['**/*.ts', '!**/__tests__/**'],
+  coverageDirectory: '../coverage',
+  coverageReporters: ['text', 'lcov'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
+
+export default config;
